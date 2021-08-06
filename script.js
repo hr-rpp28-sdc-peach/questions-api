47ae2479aba8e52799fa0bfb8168665a30df59a8
Sara Landis <saralandis5555@gmail.com>
2021-08-06 19:50:36 -0400

    Added k6 and stress test script base

---
+++ script.js
@@ -0,0 +1,21 @@
+import http from 'k6/http';
+import { sleep, check } from 'k6';
+
+
+export let options = {
+  stages: [
+    // { duration: '30s', target: 1}
+    // { duration: '1m', target: 10}
+    // { duration: '1m30s', target: 100}
+    { duration: '3m', target: 1000}
+  ]
+}
+
+export default function () {
+  for( var id = 1; id < 120; id++){
+    http.get(`http://localhost:3000/qa/questions/${id}/answers`, {
+      tags: {name: 'getAnswers'}
+    });
+  }
+  sleep(1);
+}
